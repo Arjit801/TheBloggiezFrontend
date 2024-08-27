1294fa9af07c1a2c36c53f7ee71eff337337eae1
Arjit Gupta <84505785+Arjit801@users.noreply.github.com>
2024-08-27 14:33:58 +0530

    Add files via upload

--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -58,6 +58,7 @@ const CreateBlog = () => {
         { ...body },
         {
           withCredentials: true,
+          headers: { "content-type": "multipart/form-data" }
         }
       )
       .then(function (response) {
